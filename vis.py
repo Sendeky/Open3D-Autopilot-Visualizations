5f31495e59103bbc1c1d0c656324fe222a64a45c
Sendeky <spruslasha@gmail.com>
2023-08-07 19:16:53 -0700

    added example of how the incoming data will look like (lane points)

--- vis.py
+++ vis.py
@@ -20,5 +20,12 @@ plane = o3d.geometry.TriangleMesh.create_box(width=300.0, height=5.0, depth=300.
 # print('Triangles:')
 # print(np.asarray(mesh.triangles))
 
-o3d.visualization.draw_geometries([mesh, knot_translate, plane])
+o3d.visualization.draw_geometries([knot_translate, plane])
 
+# example of give lane
+""" left_lane = [[229, 709], [238, 699], [249, 689], [260, 679], [271, 669], [282, 659],
+    [293, 649], [304, 639], [315, 629], [325, 619], [336, 609], [348, 599], [359, 589],
+    [369, 579], [381, 569], [390, 559], [404, 549], [415, 539], [426, 529], [438, 519],
+    [450, 509], [461, 499], [472, 489], [482, 479], [493, 469], [504, 458], [515, 449],
+    [526, 439], [536, 429], [546, 419], [555, 409], [565, 399], [572, 389]]
+"""
